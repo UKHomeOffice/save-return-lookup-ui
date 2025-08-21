31e73816d49e085ed0887b4b0e7a3c57ed4dc8be
Greg Austin <Gregory.Austin@digital.homeoffice.gov.uk>
2025-08-21 23:02:32 +0100

    NRM-445 Add submitted_at column * Add ability to set a submitted at time * Used to check submission worked correctly https://collaboration.homeoffice.gov.uk/jira/browse/NRM-445

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  migrationsRepo: process.env.MIGRATIONS_REPO || 'ms-schema',
+  migrationsRepo: process.env.MIGRATIONS_REPO || 'modern-slavery-schema',
   tableName: process.env.TABLE_NAME || 'reports',
   serviceType: process.env.SERVICE_TYPE || 'modern-slavery'
 };
